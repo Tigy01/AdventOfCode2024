7cbca15932e62bbf383da933abda5a4e6a0d2373
Fedora-g14 <jahumphreys2060@gmail.com>
2025-01-02 21:50:13 -0700

    day3

--- cmd/aoc/main.go
+++ cmd/aoc/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc/internal/day1"
 	"aoc/internal/day2"
+	"aoc/internal/day3"
 	"aoc/internal/day6"
 	"fmt"
 	"time"
@@ -22,6 +23,14 @@ func main() {
 	start = time.Now()
 	fmt.Println("Day2:")
 	err = day2.Run()
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+	fmt.Printf("Time Taken: %v\n\n", time.Since(start).Microseconds())
+
+	start = time.Now()
+	fmt.Println("Day3:")
+	err = day3.Run()
 	if err != nil {
 		fmt.Printf("err: %v\n", err)
 	}
--- internal/filemanager/filemanager.go
+++ internal/filemanager/filemanager.go
@@ -5,7 +5,6 @@ import (
 	"os"
 )
 
-
 func ReadLines(path string) ([]string, error) {
 	lines := make([]string, 0)
 	data, err := os.Open(path)
@@ -20,4 +19,16 @@ func ReadLines(path string) ([]string, error) {
 	return lines, nil
 }
 
-
+func ReadFullFile(path string) (output string, err error) {
+	data, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	buff := make([]byte, 1024, 1024)
+	bytes, err := data.Read(buff)
+	for err == nil {
+		output += string(buff[0:bytes])
+		bytes, err = data.Read(buff)
+	}
+    return output, nil
+}
